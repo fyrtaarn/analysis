d70c5f6ab9349d3a969e7cccff572bcb5f67d526
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-02-09 23:14:57 +0100

    Restructure test folder and files

--- npr/test-run.R
+++ npr/test-run.R
@@ -3,4 +3,6 @@ library(tinytest)
 root <- "~/Git-fhi/analysis/npr"
 source(file.path(root, "setup.R"))
 
-run_test_file(file.path(root, "test.R"))
+run_test_dir(file.path(root, "test"))
+
+run_test_file(file.path(root, "test/test-split.R"))
