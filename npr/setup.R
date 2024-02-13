76b634ee5ef502b7695d883ee7b6aa1c8acf7bff
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-02-13 14:53:07 +0100

    Add ref to the codes

--- npr/setup.R
+++ npr/setup.R
@@ -1,15 +1,15 @@
 
-
 setwd("f:/Forskningsprosjekter/PDB 3327 - Skader i Norge analy_")
 source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
 
-pkg <- c("pacman","data.table", "fst", "lubridate")
+pkg <- c("pacman","data.table", "fst", "lubridate", "ggplot2")
 kh_load(char = pkg)
 
 root <- "~/Git-fhi/analysis/npr"
 source(file.path(root, "functions.R"))
 
-# Encoding solution from
+# Encoding solution with some modification from
+# https://github.com/StoXProject/RstoxData/issues/10#issuecomment-510542301
 do_encode <- function(x) gsub("Ã¦", "æ", useBytes = TRUE,
                               gsub("Ã¥|\xe5", "å", useBytes = TRUE,
                                    gsub("Ã¸|xe6|\xf8", "ø", useBytes = TRUE,
