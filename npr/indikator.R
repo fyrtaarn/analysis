2d5befd540f88a98533c69b2f3b08941a05a4bfc
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-02-13 14:22:38 +0100

    Indicator for acute injuries

--- npr/indikator.R
+++ npr/indikator.R
@@ -1,8 +1,12 @@
+
+root <- "~/Git-fhi/analysis/npr"
+source(file.path(root, "functions.R"))
 source(file.path(root, "clean-fmds.R"))
 kb <- fread("./Data/Kodebok_Skader_i_Norge.csv", encoding = "Latin-1")
 
 # Alder
-
+dt2[, age := lubridate::year(skadeDato) - fodtAar]
+dt2 <- do_agegroup(dt2, "age", c(0, 18, 25, 45, 65, Inf))
 
 # Hardt skadd i trafikkulykker
 #-------------------------------
@@ -10,12 +14,24 @@ ais3 <- dt2[kontaktarsakSkade == 1, ][ #Ulykkesskade
   alvorlighetsgrad == 3][ #AIS 3+
     skadeSted %in% c("V1","N"),]
 
+# Kjønn
+aisKJ <- ais3[, .N, by = kjonn]
+aisKJ[, kjonn := as.character(kjonn)]
+aisKJ[kb[variabel == "kjonn",], on = c(kjonn = "kode"), beskrivelse := beskrivelse][]
 
+# Alder
+ais3[, .N, keyby = GRP]
+
+# Kjønn og Alder
+ais3[, .N, keyby = .(kjonn, GRP)]
+
+# Fremkomstmiddel
 aisFrem <- ais3[, .N, by = fremkomstmiddel]
-aisMek <- ais3[, .N, by = skadeMekanisme]
-
 aisFrem[kb[variabel == "fremkomstmiddel",], on = c(fremkomstmiddel = "kode"), beskrivelse := beskrivelse][]
-aisMek[kb[variabel == "skadeMekanisme",], on = c(skadeMekanisme = "kode"), beskrivelse := beskrivelse][]
-
 aisFrem[order(fremkomstmiddel)]
+
+
+# Skademekanisme
+aisMek <- ais3[, .N, by = skadeMekanisme]
+aisMek[kb[variabel == "skadeMekanisme",], on = c(skadeMekanisme = "kode"), beskrivelse := beskrivelse][]
 aisMek[order(skadeMekanisme)]
