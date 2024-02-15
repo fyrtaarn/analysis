9b86d4b97ed0e39e32d511b65b64b4b6b463091c
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-02-15 16:25:10 +0100

    Add col for percent

--- npr/indikator.R
+++ npr/indikator.R
@@ -1,5 +1,6 @@
 
 root <- "~/Git-fhi/analysis/npr"
+source(file.path(root, "setup.R"))
 source(file.path(root, "functions.R"))
 source(file.path(root, "clean-fmds.R"))
 kb <- fread("./Data/Kodebok_Skader_i_Norge.csv", encoding = "Latin-1")
@@ -8,40 +9,46 @@ kb <- fread("./Data/Kodebok_Skader_i_Norge.csv", encoding = "Latin-1")
 dt2[, age := lubridate::year(skadeDato) - fodtAar]
 dt2 <- do_agegroup(dt2, "age", c(0, 18, 25, 45, 65, Inf))
 
+# Koder
+# ----------
+kb[variabel == "skadeSted"]
+
 # Hardt skadd i trafikkulykker
 #-------------------------------
+skadeKB <- c("V1", "N")
 ais3 <- dt2[kontaktarsakSkade == 1, ][ #Ulykkesskade
   alvorlighetsgrad == 3][ #AIS 3+
-    skadeSted %in% c("V1","N"),]
+    skadeSted %in% skadeKB,]
 
 # Kjønn
 aisKJ <- ais3[, .N, by = kjonn]
+aisKJ[, sum := sum(N, na.rm = T)][, pro := round(100 * N/sum, 1), by = kjonn][, sum := NULL][]
 aisKJ[, kjonn := as.character(kjonn)]
 aisKJ[kb[variabel == "kjonn",], on = c(kjonn = "kode"), beskrivelse := beskrivelse][]
 
 # Alder
-ais3[, .N, keyby = GRP]
+age3 <- ais3[, .N, keyby = GRP]
+age3[, sum := sum(N, na.rm = T)][, pro := round(100 * N/sum, 1), by = GRP][, sum := NULL][]
 
 # Kjønn og Alder
 ais3[, .N, keyby = .(kjonn, GRP)]
 
 # Fremkomstmiddel
 aisFrem <- ais3[, .N, by = fremkomstmiddel]
-aisFrem[kb[variabel == "fremkomstmiddel",], on = c(fremkomstmiddel = "kode"), beskrivelse := beskrivelse][]
-aisFrem[order(fremkomstmiddel)]
+aisFrem[kb[variabel == "fremkomstmiddel",], on = c(fremkomstmiddel = "kode"), beskrivelse := beskrivelse][
+  order(fremkomstmiddel)]
 
 
 # Skademekanisme
 aisMek <- ais3[, .N, by = skadeMekanisme]
-aisMek[kb[variabel == "skadeMekanisme",], on = c(skadeMekanisme = "kode"), beskrivelse := beskrivelse][]
-aisMek[order(skadeMekanisme)]
+aisMek[kb[variabel == "skadeMekanisme",], on = c(skadeMekanisme = "kode"), beskrivelse := beskrivelse][order(skadeMekanisme)]
 
 
 # Lettere skadd i trafikkulykker
 # ------------------------------
 ais2 <- dt2[kontaktarsakSkade == 1, ][ #Ulykkesskade
   alvorlighetsgrad %in% 1:2][ #AIS 1 & 2
-    skadeSted %in% c("V1","N"),]
+    skadeSted %in% skadeKB]
 
 # Kjønn
 aisKJ2 <- ais2[, .N, by = kjonn]
@@ -56,11 +63,9 @@ ais2[, .N, keyby = .(kjonn, GRP)]
 
 # Fremkomstmiddel
 aisFrem <- ais2[, .N, by = fremkomstmiddel]
-aisFrem[kb[variabel == "fremkomstmiddel",], on = c(fremkomstmiddel = "kode"), beskrivelse := beskrivelse][]
-aisFrem[order(fremkomstmiddel)]
+aisFrem[kb[variabel == "fremkomstmiddel",], on = c(fremkomstmiddel = "kode"), beskrivelse := beskrivelse][order(fremkomstmiddel)]
 
 
 # Skademekanisme
 aisMek <- ais2[, .N, by = skadeMekanisme]
-aisMek[kb[variabel == "skadeMekanisme",], on = c(skadeMekanisme = "kode"), beskrivelse := beskrivelse][]
-aisMek[order(skadeMekanisme)]
+aisMek[kb[variabel == "skadeMekanisme",], on = c(skadeMekanisme = "kode"), beskrivelse := beskrivelse][order(skadeMekanisme)]
