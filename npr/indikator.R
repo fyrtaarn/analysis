c277ade5ae462f5036d98538e6c2f85bd5d7319a
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-02-20 16:52:17 +0100

    Use functions and house keeping

--- npr/indikator.R
+++ npr/indikator.R
@@ -16,59 +16,54 @@ sted <- dt2[, .N, by = skadeSted]
 stedTBL <- show_pro(dt2, "skadeSted", kb)
 stedTBL
 
-# Hardt skadd i trafikkulykker
-#-------------------------------
 skadeKB <- c("V1", "N")
+
+# -----------------------------------
+# Hardt skadd i trafikkulykker AIS 3+
+# -----------------------------------
 ais3 <- dt2[kontaktarsakSkade == 1, ][ #Ulykkesskade
   alvorlighetsgrad == 3][ #AIS 3+
     skadeSted %in% skadeKB,]
 
-# Kjønn
+# Kjønn AIS3+
 kjonnTBL3 <- show_pro(ais3, "kjonn", kb)
 
-# Alder
+# Alder AIS3+
 ageTBL3 <- show_pro(ais3, "GRP")
 
 # Kjønn og Alder
 ais3[, .N, keyby = .(kjonn, GRP)]
 
+
 # Koder for fremkomstmiddel (OBS! mange feil rapportert)
 # --------------------------
-fremTBL3 <- show_pro(dt2, "fremkomstmiddel", kb)
+fremTBL <- show_pro(dt2, "fremkomstmiddel", kb)
 
-# Fremkomstmiddel
-aisFrem <- ais3[, .N, by = fremkomstmiddel]
-aisFrem[kb[variabel == "fremkomstmiddel",], on = c(fremkomstmiddel = "kode"), beskrivelse := beskrivelse][
-  order(fremkomstmiddel)]
+# Fremkomstmiddel AIS3+
+fremTBL3 <- show_pro(ais3, "fremkomstmiddel", kb)
 
-
-# Skademekanisme
-aisMek <- ais3[, .N, by = skadeMekanisme]
-aisMek[kb[variabel == "skadeMekanisme",], on = c(skadeMekanisme = "kode"), beskrivelse := beskrivelse][order(skadeMekanisme)]
+# Skademekanisme AIS3+
+mekTBL3 <- show_pro(ais3, "skadeMekanisme", kb)
 
 
-# Lettere skadd i trafikkulykker
-# ------------------------------
+# ----------------------------------------
+# Lettere skadd i trafikkulykker AIS 1 & 2
+# ----------------------------------------
 ais2 <- dt2[kontaktarsakSkade == 1, ][ #Ulykkesskade
   alvorlighetsgrad %in% 1:2][ #AIS 1 & 2
     skadeSted %in% skadeKB]
 
 # Kjønn
-aisKJ2 <- ais2[, .N, by = kjonn]
-aisKJ2[, kjonn := as.character(kjonn)]
-aisKJ2[kb[variabel == "kjonn",], on = c(kjonn = "kode"), beskrivelse := beskrivelse][]
+kjonnTBL2 <- show_pro(ais2, "kjonn", kb)
 
 # Alder
-ais2[, .N, keyby = GRP]
+ageTBL2 <- show_pro(ais2, "GRP")
 
 # Kjønn og Alder
 ais2[, .N, keyby = .(kjonn, GRP)]
 
 # Fremkomstmiddel
-aisFrem <- ais2[, .N, by = fremkomstmiddel]
-aisFrem[kb[variabel == "fremkomstmiddel",], on = c(fremkomstmiddel = "kode"), beskrivelse := beskrivelse][order(fremkomstmiddel)]
-
+fremTBL2 <- show_pro(ais2, "fremkomstmiddel", kb)
 
 # Skademekanisme
-aisMek <- ais2[, .N, by = skadeMekanisme]
-aisMek[kb[variabel == "skadeMekanisme",], on = c(skadeMekanisme = "kode"), beskrivelse := beskrivelse][order(skadeMekanisme)]
+mekTBL2 <- show_pro(ais2, "skadeMekanisme", kb)
