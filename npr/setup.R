a88645b840ee8ef792e25c5c4e4c0ce61f0f64d9
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-02-26 14:05:20 +0100

    Add chosen hosp and emergency units

--- npr/setup.R
+++ npr/setup.R
@@ -6,7 +6,9 @@ pkg <- c("pacman","data.table", "fst", "lubridate", "ggplot2", "plotly")
 kh_load(char = pkg)
 
 root <- "~/Git-fhi/analysis/npr"
-source(file.path(root, "./functions/utils.R"))
+fx <- list.files(file.path(root, "functions"))
+for (i in fx)
+  source(file.path(root, "functions", i))
 
 # Encoding solution with some modification from
 # https://github.com/StoXProject/RstoxData/issues/10#issuecomment-510542301
@@ -16,3 +18,33 @@ do_encode <- function(x) gsub("Ã¦", "æ", useBytes = TRUE,
                                         gsub("\xed", "i", useBytes = TRUE,
                                              gsub("\xc5", "Å", useBytes = TRUE,
                                                   gsub("\xd8", "Ø", x, useBytes = TRUE))))))
+
+
+kb <- fread("./Data/Kodebok_Skader_i_Norge.csv", encoding = "Latin-1")
+# kb[variabel %like% "^Org"][beskrivelse %like% "OUS"][, .(beskrivelse, kode)]
+sykehus <- c(
+  974795639, #UNN Harstad
+  974795787, #UNN Tromsø
+  883974832, #St. Olavs Hospital HF
+  974749025, #St. Olavs hospital
+  983975240, #Sørlandet Sykehus HF
+  974633574, #Sykehuset i Vestfold
+  983975259, #Sykehuset i Vestfold HF
+  983971709, #Sykehuset Innlandet HF
+  974631768, #SI Elverum - Hamar
+  874632562, #SI Lillehammer
+  974589095, #OUS somatikk
+  993467049 #Oslo universitetssykehus HF
+  # ??? OUS Skadelegevakten
+)
+
+legevakt <- c(
+  993187178, #Trondheim legevakt
+  998563143, #Hedmarken interkomm. legevakt
+  927161222, #Hedmarken interkomm. legevakt
+  998580072, #Lillehammer interkomm. legevakt
+  974057344 #Tromsø Komm. Legevakt
+  # Kristiansand legevakt
+  # Tønsberg-regionen legevakt
+  # Bergen legevakt
+)
