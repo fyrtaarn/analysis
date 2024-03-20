1ca65996e0e44012dd4e125c4f2ff9f511968bf4
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-03-20 11:19:29 +0100

    Feat: Keep only 3 digits in ICD10 codes

--- npr/functions/utils.R
+++ npr/functions/utils.R
@@ -2,14 +2,15 @@
 
 #' Select codes from column consisting ICD-10 codes that meet the requirement for injury codes ie. between S00 to T78
 #' @param d Dataset
-#' @param select.col Columnames from dataset with ICD codes to be controlled for
+#' @param select.col Columnames from dataset with ICD codes to be controlled for. Default is `hoveddiagnoser`
 #' @param create.col New columnames consisting logical value to indicate that at least one
 #' of these codes S00 to T78 exists ie. TRUE means one or more of the codes in `select.col`
-#' is between S00 to T78
+#' is between S00 to T78. Default is `hovdiag`
 #' @param split Symbols for splitting the codes when there are multiple codes in the column
+#' @return Original column will have suffix `*.old`
 #' @examples
 #' d1 <- get_valid_codes(dt = dd, "hoveddiagnoser", "hovdiag")
-get_valid_codes <- function(d, select.col, create.col, split = " "){
+get_valid_codes <- function(d, select.col = "hoveddiagnoser", create.col = "hovdiag", split = " "){
 
   dx <- data.table::copy(d)
 
@@ -23,16 +24,27 @@ get_valid_codes <- function(d, select.col, create.col, split = " "){
   cols <- paste0("temp", 1:max(dx$colnr))
   dx[, (cols) := data.table::tstrsplit(x = col1, " "), env = list(col1 = select.col)]
 
+  # Trim codes to keep only the first 3 digits
+  for (j in cols){
+    if(is.character(dx[[j]]))
+      data.table::set(dx, j = j, value = substr(dx[[j]], 1, 3))
+  }
+
+  tempCol <- paste0(select.col, "01")
+  dx[, (tempCol) := do.call(paste, c(replace(.SD, is.na(.SD), ""), sep = " ")), .SDcols = cols]
+
   # Select only these codes S00 til T78
   codeURL <- "https://github.com/fyrtaarn/analysis/raw/main/data/validCodes.RDS"
   codes <- readRDS(url(codeURL))
 
   for (j in cols){
     if(class(dx[[j]]) == 'character')
-      set(dx, j = j, value = substr(dx[[j]], 1, 3) %chin% codes)
+      set(dx, j = j, value = dx[[j]] %chin% codes)
   }
 
   dx[ , (create.col) := rowSums(.SD) > 0, .SDcols = cols]
+  oldCol <- paste0(select.col, ".old")
+  data.table::setnames(dx, old = c(select.col, tempCol), new = c(oldCol, select.col))
   xcols <- c("colnr", "lnr", cols)
   dx[, (xcols) := NULL][]
 }
