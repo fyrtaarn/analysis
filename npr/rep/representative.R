4a9e7e0770e37af6d846858780131b0dbd36142d
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2025-03-04 17:45:57 +0100

    Fix: Diagnose for forgiftning

--- npr/rep/representative.R
+++ npr/rep/representative.R
@@ -64,7 +64,8 @@ show_pro(fm,"agegp")
 
 ## ICD10 Diagnoser
 ## ----------------------------
-icd <- fyr::get_valid_codes(som, keep = TRUE)
+somm <- caseN3[!is.na(lopenr) & dup != 1 & Ft_dummy_Spesialist == 1,]
+icd <- fyr::get_valid_codes(somm, keep = TRUE)
 
 icd[, icd := fcase(icd_1 %chin% paste0("S0", 0:9), 0,
                    icd_1 %chin% paste0("S1", 0:9), 1,
@@ -83,14 +84,41 @@ icd[, icd := fcase(icd_1 %chin% paste0("S0", 0:9), 0,
                    icd_1 %chin% paste0("T2", 0:9), 13, #Brannskader
                    icd_1 %chin% paste0("T3", 0:2), 13, #Brannskader
                    icd_1 %chin% paste0("T3", 3:5), 14, #Frostskade
-                   icd_1 %chin% paste0("T4", c(1, "n", 0)), 15, #Forgiftning
+                   icd_1 %chin% paste0("T4", c(1, "n")), 15, #Forgiftning
+                   icd_1 %chin% "T50", 15, #Forgiftning
                    icd_1 %chin% paste0("T5", 1:9), 16, #Toksiske virkninger
                    icd_1 %chin% paste0("T6", 0:5), 16, #Toksiske virkninger
                    icd_1 %chin% paste0("T6", 6:9), 17, #Annet
                    icd_1 %chin% paste0("T7", 0:8), 17 #Annet
                    )]
 
+## Use icd 2 if necessary
+icd[is.na(icd), icd := fcase(icd_2 %chin% paste0("S0", 0:9), 0,
+                   icd_2 %chin% paste0("S1", 0:9), 1,
+                   icd_2 %chin% paste0("S2", 0:9), 2,
+                   icd_2 %chin% paste0("S3", 0:9), 3,
+                   icd_2 %chin% paste0("S4", 0:9), 4,
+                   icd_2 %chin% paste0("S5", 0:9), 5,
+                   icd_2 %chin% paste0("S6", 0:9), 6,
+                   icd_2 %chin% paste0("S7", 0:9), 7,
+                   icd_2 %chin% paste0("S8", 0:9), 8,
+                   icd_2 %chin% paste0("S9", 0:9), 9,
+                   icd_2 %chin% paste0("T0", 0:7), 10,
+                   icd_2 %chin% paste0("T0", 8:9), 11, #Uspesifisert
+                   icd_2 %chin% paste0("T", 10:14), 11, #Uspesifisert
+                   icd_2 %chin% paste0("T1", 5:9), 12, # Fremmedlegeme
+                   icd_2 %chin% paste0("T2", 0:9), 13, #Brannskader
+                   icd_2 %chin% paste0("T3", 0:2), 13, #Brannskader
+                   icd_2 %chin% paste0("T3", 3:5), 14, #Frostskade
+                   icd_2 %chin% paste0("T4", c(1, "n")), 15, #Forgiftning
+                   icd_2 %chin% "T50", 15, #Forgiftning
+                   icd_2 %chin% paste0("T5", 1:9), 16, #Toksiske virkninger
+                   icd_2 %chin% paste0("T6", 0:5), 16, #Toksiske virkninger
+                   icd_2 %chin% paste0("T6", 6:9), 17, #Annet
+                   icd_2 %chin% paste0("T7", 0:8), 17 #Annet
+                   )]
 
+## Kodebok må ha disse 3 variablene
 icdkode <- data.table(kode = 0:17,
                       variabel = "icd",
                       beskrivelse = c("Hode",
@@ -112,7 +140,5 @@ icdkode <- data.table(kode = 0:17,
                                    "Toksiske virkninger",
                                    "Annet"))
 
-## Må være character for å kunne bruke som kodebok
-icdkode[, kode := as.character(kode)]
 
 show_pro(icd, "icd", code = icdkode)
